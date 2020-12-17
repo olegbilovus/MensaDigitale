54e13795470ccfeb9c752ee78ef802fd5157e930
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2020-12-17 10:21:34 +0100

    added sonarlint folder to gitignore and a taste class for branch test

---
+++ mensadigitale/src/main/java/business/prenotazioni/Test.java
@@ -0,0 +1,14 @@
+package business.prenotazioni;
+
+public class Test {
+
+  public void test() {
+    String a = "a";
+
+    if (a.equals("b")) {
+      a = "a";
+    } else {
+      a = "b";
+    }
+  }
+}
