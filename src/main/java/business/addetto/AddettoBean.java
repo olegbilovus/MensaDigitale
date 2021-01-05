a4910a78da43a8d6035b5a4b24f7ee318ceee60a
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2021-01-05 16:51:32 +0100

    deleted equals and hash

--- src/main/java/business/addetto/AddettoBean.java
+++ src/main/java/business/addetto/AddettoBean.java
@@ -70,57 +70,6 @@ public class AddettoBean {
 
 
   @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((cognome == null) ? 0 : cognome.hashCode());
-    result = prime * result + ((email == null) ? 0 : email.hashCode());
-    result = prime * result + lvlPermessi;
-    result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-    return result;
-  }
-
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    AddettoBean other = (AddettoBean) obj;
-    if (cognome == null) {
-      if (other.cognome != null) {
-        return false;
-      }
-    } else if (!cognome.equals(other.cognome)) {
-      return false;
-    }
-    if (email == null) {
-      if (other.email != null) {
-        return false;
-      }
-    } else if (!email.equals(other.email)) {
-      return false;
-    }
-    if (lvlPermessi != other.lvlPermessi) {
-      return false;
-    }
-    if (nome == null) {
-      if (other.nome != null) {
-        return false;
-      }
-    } else if (!nome.equals(other.nome)) {
-      return false;
-    }
-    return true;
-  }
-
-  @Override
   public String toString() {
     return "AddettoBean [email=" + email + ", nome=" + nome + ", cognome=" + cognome
         + ", lvlPermessi=" + lvlPermessi + "]";
