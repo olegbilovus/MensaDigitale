c7a52211ec0eec0509e13bcf5bfb2efaaee5761f
Simasu <73019961+Simasu@users.noreply.github.com>
2021-01-13 00:40:06 +0100

    Aggiunti 3 script sql per popolare il db

---
+++ scriptDatabase/PopulateConsumatori.sql
@@ -0,0 +1,9 @@
+USE mensadigitale;
+INSERT INTO consumatore VALUES ('email1', 'Simone', 'Masullo', 0, 'AAA', null, null, null, null, null, null, null, false, false, 0, 1),
+								('email2', 'Antonio', 'Cacciapuoti', 0, 'BBB', null, null, null, null, null, null, null, false, false, 0, 1),
+                                ('email3', 'Oleg', 'Bilovus', 0, 'CCC', null, null, null, null, null, null, null, false, false, 0, 1),
+                                ('email4', 'Raffaele', 'Squillante', 0, 'DDD', null, null, null, null, null, null, null, false, false, 0 , 1),
+                                ('email5', 'Antonio', 'Giametta', 0, 'EEE', null, null, null, null, null, null, null, false, false, 0 , 1),
+                                ('email6', 'Paolo', 'Pisapia', 0, 'FFF', null, null, null, null, null, null, null, false, false, 0, 1),
+                                ('email', 'Francesco', 'Capriglione', 1, 'GGG', null, null, null, null, null, null, null, false, false, 5000, 1);
+
