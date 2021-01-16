3f96937c78568b682ec996466163a76b9c0fbe4e
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2021-01-16 16:54:44 +0100

    Update scriptTravis.sh

--- scriptTravis.sh
+++ scriptTravis.sh
@@ -4,8 +4,8 @@ cd $TRAVIS_BUILD_DIR
 git pull origin HEAD:main
 rm -rf docs/jacoco-resources
 rm -rf docs/index.html
-mv $TRAVIS_BUILD_DIR/target/site/jacoco/index.html ./docs
-mv $TRAVIS_BUILD_DIR/target/site/jacoco/jacoco-resources ./docs
+mv $TRAVIS_BUILD_DIR/target/site/jacoco ./docs
+#mv $TRAVIS_BUILD_DIR/target/site/jacoco/jacoco-resources ./docs
 ls ./docs
 TZ=Europe/Rome date >> ./docs/index.html
 git add docs
