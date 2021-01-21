6556897ba3cad0443cfa92c8beaafd181bdb4c27
Antonio Cacciapuoti <yantcaccia@gmail.com>
2021-01-21 16:16:36 +0100

    Update PopulateConsumatori.sql
    date di nascita se no va in crash

--- scriptDatabase/PopulateConsumatori.sql
+++ scriptDatabase/PopulateConsumatori.sql
@@ -1,10 +1,10 @@
 USE mensadigitale;
-INSERT INTO consumatore VALUES ('email1', 'Simone', 'Masullo', 0, 'AAA', null, null, null, null, null, null, null, false, false, 0, 1),
-								('email2', 'Antonio', 'Cacciapuoti', 0, 'BBB', null, null, null, null, null, null, null, false, false, 0, 1),
-                                ('email3', 'Oleg', 'Bilovus', 0, 'CCC', null, null, null, null, null, null, null, false, false, 0, 1),
-                                ('email4', 'Raffaele', 'Squillante', 0, 'DDD', null, null, null, null, null, null, null, false, false, 0 , 1),
-                                ('email5', 'Antonio', 'Giametta', 0, 'EEE', null, null, null, null, null, null, null, false, false, 0 , 1),
-                                ('email6', 'Paolo', 'Pisapia', 0, 'FFF', null, null, null, null, null, null, null, false, false, 0, 1),
-                                ('email', 'Francesco', 'Capriglione', 1, 'GGG', null, null, null, null, null, null, null, false, false, 5000, 1),
-                                ('a.cacciapuoti3@studenti.unisa.it', 'Antonio', 'Cacciapuoti', 1, 'CCCNTN999999999X', null, null, null, null, null, null, null, null, null, 100, 1);
+INSERT INTO consumatore VALUES ('email1', 'Simone', 'Masullo', 0, 'AAA', '2000-03-26', null, null, null, null, null, null, false, false, 0, 1),
+								('email2', 'Antonio', 'Cacciapuoti', 0, 'BBB', '1999-10-06', null, null, null, null, null, null, false, false, 0, 1),
+                                ('email3', 'Oleg', 'Bilovus', 0, 'CCC', '1999-01-01', null, null, null, null, null, null, false, false, 0, 1),
+                                ('email4', 'Raffaele', 'Squillante', 0, 'DDD', '1999-01-01', null, null, null, null, null, null, false, false, 0 , 1),
+                                ('email5', 'Antonio', 'Giametta', 0, 'EEE', '1999-01-01', null, null, null, null, null, null, false, false, 0 , 1),
+                                ('email6', 'Paolo', 'Pisapia', 0, 'FFF', '1999-01-01', null, null, null, null, null, null, false, false, 0, 1),
+                                ('email', 'Francesco', 'Capriglione', 1, 'GGG', '1997-10-06', null, null, null, null, null, null, false, false, 5000, 1),
+                                ('a.cacciapuoti3@studenti.unisa.it', 'Antonio', 'Cacciapuoti', 1, 'CCCNTN999999999X', '1999-10-06', null, null, null, null, null, null, null, null, 100, 1);
 
