6f70ce7293d964a4f5b0b40f9c58e4fb88b90276
Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>
2021-07-21 20:36:46 +0200

    Updated script to show all files in the coverage Pages

--- scriptTravis.sh
+++ scriptTravis.sh
@@ -2,10 +2,8 @@
 
 cd $TRAVIS_BUILD_DIR
 git pull origin HEAD:main
-rm -rf docs/jacoco-resources
-rm -rf docs/index.html
-mv $TRAVIS_BUILD_DIR/target/site/jacoco/index.html ./docs
-mv $TRAVIS_BUILD_DIR/target/site/jacoco/jacoco-resources ./docs
+rm -rf docs/*
+mv $TRAVIS_BUILD_DIR/target/site/jacoco/* ./docs
 ls ./docs
 TZ=Europe/Rome date >> ./docs/index.html
 git add docs
